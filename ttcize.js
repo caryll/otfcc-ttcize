5bb72ed9e40320d064d7144ca2faa667ea2de284
be5invis <belleve@typeof.net>
2017-06-23 19:13:06 +0800

    added -h flag for hint merging

--- ttcize.js
+++ ttcize.js
@@ -6,45 +6,79 @@ const stringifyToStream = require("./stringify-to-stream");
 
 temp.track();
 
-void function () {
+void (function() {
 	let fonts = [];
 	let glyf = {};
 	let gmap = {};
 	let ix = 0;
+	let maxcvt = 0;
 	for (let f of argv._) {
 		let p = temp.path();
 		let prefix = `s.`;
 		//let prefix = `s${fonts.length}.`;
 		console.log(`Reading font ${f}`);
-		child_process.execSync(`otfccdump ${f} -o ${p} --name-by-hash --glyph-name-prefix=${prefix}`);
+		child_process.execSync(
+			`otfccdump ${f} -o ${p} --name-by-hash --glyph-name-prefix=${prefix}`
+		);
 		let font = JSON.parse(fs.readFileSync(p));
 		fs.unlinkSync(p);
 		let n = 0;
 		for (let gid in font.glyf) {
 			if (!glyf[gid]) {
 				glyf[gid] = font.glyf[gid];
-				gmap[gid] = [ix, n]
+				gmap[gid] = { firstInstID: ix, firstInstGID: n, used: [ix] };
 			} else {
 				font.glyf[gid] = glyf[gid];
+				gmap[gid].used.push(ix);
 			}
 			n += 1;
 		}
 		font.glyf = glyf;
 		fonts.push(font);
-		if (global.gc) { global.gc() }
+		if (global.gc) {
+			global.gc();
+		}
+		if (argv.h && font.cvt_) {
+			maxcvt = Math.max(font.cvt_.length, maxcvt);
+		}
+		font.$ix = ix;
 		ix += 1;
 	}
-
-	let keys = Object.keys(gmap).sort(function (a, b) {
+	let keys = Object.keys(gmap).sort(function(a, b) {
 		const p = gmap[a], q = gmap[b];
-		return p[0] === q[0] ? p[1] - q[1] : p[0] - q[0];
+		return p.firstInstID === q.firstInstID
+			? p.firstInstGID - q.firstInstGID
+			: p.firstInstID - q.firstInstID;
 	});
 	process.stderr.write(`${keys.length} unique glyphs after merging.` + "\n");
-
-	for (let f of fonts) { f.glyph_order = keys }
+	if (argv.h) {
+		for (let g in glyf) {
+			if (!glyf[g] || !gmap[g] || !glyf[g].instructions) continue;
+			if (gmap[g].used.length === ix) continue;
+			let head = [];
+			for (let j = 0; j < gmap[g].used.length; j++) {
+				let ixj = gmap[g].used[j];
+				head.push("PUSHW_2", ixj, maxcvt, "RCVT", "EQ");
+				if (j > 0) head.push("OR");
+			}
+			glyf[g].instructions = [...head, "IF", ...glyf[g].instructions, "EIF"];
+		}
+	}
+	for (let f of fonts) {
+		f.glyph_order = keys;
+		if (argv.h && f.cvt_) {
+			while (f.cvt_.length < maxcvt) {
+				f.cvt_.push(0);
+			}
+			f.cvt_[maxcvt] = f.$ix;
+		}
+		if (argv.h) {
+			f.maxp.maxStackElements += 2;
+		}
+	}
 
 	writeOTDs(fonts, finalize)();
-}();
+})();
 
 function writeOTDs(fonts, callback) {
 	let current = 0;
@@ -57,29 +91,33 @@ function writeOTDs(fonts, callback) {
 		let pOTD = temp.path();
 		let pTTF = temp.path();
 		let outstream = fs.createWriteStream(pOTD, { encoding: "utf-8" });
-		stringifyToStream(fonts[current], outstream, false)(function () {
-			console.log(`Temp font ${current} successfully built.`);
+		stringifyToStream(fonts[current], outstream, false)(function() {
 			current += 1;
 			otds.push(pOTD);
 			ttfs.push(pTTF);
-			if (global.gc) { global.gc(); }
+			if (global.gc) {
+				global.gc();
+			}
 			setTimeout(step, 0);
-		})
+		});
 	}
 	return step;
 }
 
 function finalize(input) {
 	const { otds, ttfs } = input;
-	if (global.gc) { global.gc(); }
+	if (global.gc) {
+		global.gc();
+	}
 
 	for (let j = 0; j < otds.length; j++) {
 		let pOTD = otds[j], pTTF = ttfs[j];
 		child_process.execSync(`otfccbuild ${pOTD} -o ${pTTF} -O3 -k --keep-average-char-width`);
+		console.log(`Temp font ${j} successfully built.`);
 		fs.unlinkSync(pOTD);
 	}
 
-	child_process.execSync(`otf2otc ${ttfs.join(' ')} -o ${argv.o}`);
+	child_process.execSync(`otf2otc ${ttfs.join(" ")} -o ${argv.o}`);
 	for (let p of ttfs) {
 		fs.unlinkSync(p);
 	}
